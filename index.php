cc2418384cd667a54144e0e6a4d41657171a821f
Marlow Collins <marlowcollins95@gmail.com>
2022-04-08 15:02:10 -0500

    updates

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <h1>Testing DB</h1>
 <div>
     <?php
+        $dsn = 'mysql:host=127.0.0.1;port=3306;dbname=testdb;';
+        $user = 'marlowc';
+        $pass = '';
+        $pdo = new PDO($dsn, $user, $pass);
+        
+        //Test exception for nonsense data
         try{
+            echo('<h5>' . "Connection Open" . '</h5>'); 
+            $stmt =  $pdo->query('SELECT * FROM Users');
+            foreach($stmt as $data) print_r($data['UserName'] . "</br>");
 
-            $pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=testdb;');
-            $stmt = $pdo->query("SELECT * FROM Users");
-
-            while($row = $pdo->fetch(PDO::FETCH_ASSOCIATE)){
-                print_r($row);
-            }
-            
         } catch(PDOException $e){
     
-            echo $e->getMessage();
+            echo($e->getMessage());
         }
+
     
     ?>
 </div>
